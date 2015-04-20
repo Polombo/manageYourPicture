31b5e986aba193d3cbedb83d5f978badc7053284
polombo <elpolombo@gmail.com>
2015-04-20 20:37:07 +0200

    Added new SQL script and Instructions.txt

--- bbdd/manage_your_picture.sql
+++ bbdd/manage_your_picture.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 20, 2015 at 01:41 PM
+-- Generation Time: Apr 20, 2015 at 08:31 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `administradores` (
 --
 
 INSERT INTO `administradores` (`id_administrador`, `nombre_admin`, `contrasenia`) VALUES
-(1, 'admin', 'b1902a1e743ed2fa0693569ce72c2fb2');
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
 -- --------------------------------------------------------
 
@@ -54,20 +54,19 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(50) DEFAULT NULL,
   `sign_in_date` datetime NOT NULL,
   `terms` enum('yes','no') DEFAULT 'no',
-  `upload_date` date DEFAULT NULL,
+  `upload_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   `url_image` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `unique_name` (`id`,`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `sign_in_date`, `terms`, `upload_date`, `url_image`) VALUES
-(1, 'Polo mbo &#40;Polombo&#41;', 'elpolombo@gmail.com', '2015-04-20 12:09:49', 'yes', NULL, './uploads/images/upload/elpolombo@gmail.com/1940BG.png'),
-(2, 'Pollino', 'elpollino@gmail.com', '0000-00-00 00:00:00', 'no', NULL, NULL);
+(1, 'Polo mbo &#40;Polombo&#41;', 'elpolombo@gmail.com', '2015-04-20 19:57:19', 'yes', '2015-04-20 18:28:57', './uploads/images/upload/elpolombo@gmail.com/BrushedMetal.png');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
